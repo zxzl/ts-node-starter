1ad010567f5821aaf7b4373faada940fcbea9cf2
Hyeungshik Jung <hyungshike@gmail.com>
2019-03-31 16:32:00 +0900

    Add debugger setting

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,6 @@ WORKDIR /usr/app
 COPY package.json ./
 RUN yarn
 
+EXPOSE 9229
 EXPOSE 3000
 CMD ["yarn", "watch"]
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import Koa from 'koa'
 const app = new Koa()
 
+let count = 1
+
 app.use(async ctx => {
-    ctx.body = 'Hello World'
+    console.log(count++)
+    ctx.body = `Hello World Europe ${count}`
 })
 
 app.listen(3000)
