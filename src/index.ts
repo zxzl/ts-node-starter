19101877ccdf829de3b91db47feb4c9e92e512e1
Hyeungshik Jung <hyungshike@gmail.com>
2019-03-31 13:47:55 +0900

    Config eslint and prettier

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import Koa from "koa";
-const app = new Koa();
+import Koa from 'koa'
+const app = new Koa()
 
 app.use(async ctx => {
-  ctx.body = "Hello World";
-});
+    ctx.body = 'Hello World'
+})
 
-app.listen(3000);
+app.listen(3000)
